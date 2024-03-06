cf9283889cc9d3be3abbac646abd6ea7f4d6eb6e
Chaehyeong Lee <98090831+ChaehyeongLee@users.noreply.github.com>
2024-03-06 19:50:43 +0900

    Update thermal_memory_ar1.py
    thermal memory main function is improved by employing xarray and vectorization

--- analysis/thermal_memory_ar1.py
+++ analysis/thermal_memory_ar1.py
@@ -5,40 +5,48 @@ This library was written to calculate surface theraml memory from sea surface te
 It also includes functions for quantitatively deriving changes in time-series data using the Mann-Kendall trend test and the Theil-sen trend test.
 '''
 import numpy as np
+import xarray as xr
 import pymannkendall as mk
 from numba import jit
 
 '''
-Input   detrend_ssta: one dimensional detrended SST anomalies
+Input   Sea surface temperature time series data (not less than 1 dimension)
+
 '''
 @jit(nopython=True)
-def autocorr_lag1(x): # lag-1 autocorrelation
-    if np.isnan(np.sum(x)): result = np.nan 
-    else: result = np.corrcoef(x[:-1], x[1:])[0,1]
-    return result
+def memory_timescale(da,vars, option='annual'):
+    da = da[vars]
+    tau_func = lambda x: -1/np.log(np.corrcoef(x[:-1],x[1:])[0,1])
 
-
-def add_lag0(lag_1): # add lag-0 autocorrelation(1) in front of lag-1 autocorrelation.
-    len_lat = lag_1.shape[0]
-    len_lon = lag_1.shape[1]
-    result = np.ones([2,len_lat,len_lon])
-    result[1,:,:] = lag_1
-    return result
-
-def model_exp(x,coeff): # simple exponentially decaying function
-    return np.exp(-coeff*x)
-
-
-def memory_timescale(autocorr_coeff): 
-    if np.isnan(np.sum(autocorr_coeff)): persist, half_persist = np.nan, np.nan
+    if option=='annual': 
+        annual_da = da.groupby('time.year')
+        tau = annual_da.apply(
+            lambda y: xr.apply_ufunc(
+                lambda z: np.apply_along_axis(tau_func, 0, z),
+                y,
+                dask='parallelized',
+                input_core_dims=[['time']], 
+                output_core_dims=[[]],  
+                output_dtypes=[float],
+                vectorize=True
+            )
+        )
     else: 
-        lags=np.array([0,1])
-        persist = -1/np.log(autocorr_coeff)
-        half_persist = -(1-np.log(2))/np.log(autocorr_coeff)
-    return persist, half_persist
+        tau = xr.apply_ufunc(
+            lambda y: np.apply_along_axis(tau_func,0,y),
+            da,
+            dask='parallelized',
+            input_core_dims=[['time']],
+            output_core_dims=[[]],
+            output_dtypes=[float],
+            vectorize=True
+        )
+    
+    return tau
 
 #
 # Mann-Kendall Trend test (modifie from Hussain et al.,(2019))
+# slope follows Theil-Sen method
 #
 def trend_test(x):
     if np.isnan(np.sum(x)): trend, slope = np.nan, np.nan
